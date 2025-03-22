f02956d03c8a37de84350f9940fb4ed89f2c4d00
danilo <danilotvillamizar@gmail.com>
2025-03-21 21:38:11 -0400

    Ajustes en la estructura

--- core/erp/models.py
+++ core/erp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from datetime import datetime
 
+from django.forms import model_to_dict
+
+from core.erp.choices import gender_choices
+
 # Create your models here.
 
 class Type(models.Model):
@@ -16,10 +20,20 @@ class Type(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=150, verbose_name='Nombre')
+    name = models.CharField(max_length=150, verbose_name='Nombre', unique=True)
+    desc = models.CharField(max_length=500, null=True, blank=True, verbose_name='Descripción')
 
     def __str__(self):
         return self.name
+
+    def toJSON(self):
+        item = model_to_dict(self)
+        return item
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['id']
 
     class Meta:
         verbose_name = 'Categoría'
